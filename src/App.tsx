8a25f86b256a179182444f0d1755a510870a0bbe
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-23 15:43:39 +0600

    Added extra buttons to Modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import Modal from './components/Modal/Modal';
 function App() {
   const [showModal, setShowModal] = useState(false);
 
+  const modalButtons = [
+    {
+      type: 'primary',
+      label: 'Continue',
+      onClick: () => console.log('clicked continue'),
+    },
+    {
+      type: 'danger',
+      label: 'Close',
+      onClick: () => cancel(),
+    },
+  ];
+
   const cancel = () => {
     setShowModal(false);
   };
@@ -20,6 +33,18 @@ function App() {
       <Modal show={showModal} onClose={cancel} title='Modal title'>
         <div className='modal-body'>
           <p>This is modal content</p>
+        </div>
+        <div className='modal-footer'>
+          {modalButtons.map((btn) => (
+            <button
+              type='button'
+              className={`btn btn-${btn.type}`}
+              onClick={btn.onClick}
+              key={Math.random()}
+            >
+              {btn.label}
+            </button>
+          ))}
         </div>
       </Modal>
     </div>
